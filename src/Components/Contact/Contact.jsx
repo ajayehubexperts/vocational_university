acf601531738fdb888cab60d3668175b913cf9bc
Ajay Sahani <ajay.sahani@ehubexperts.com>
2024-07-16 15:00:18 +0530

    complete footer

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,8 +1,37 @@
 import React from "react";
 import "./Contact.css";
 import msg_icon from "../../assets/msg-icon.png";
+import mail_icon from "../../assets/mail-icon.png";
+import phone_icon from "../../assets/phone-icon.png";
+import location_icon from "../../assets/location-icon.png";
+import white_arrow from "../../assets/white-arrow.png";
 
 const Contact = () => {
+  const [resultMessage, setResultMessage] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResultMessage("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "7f86a747-4fc1-483a-8d8e-2d31560c2b76");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResultMessage("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResultMessage(data.message);
+    }
+  };
+
   return (
     <div className="contact">
       <div className="contact-col">
@@ -16,12 +45,50 @@ const Contact = () => {
           university community.
         </p>
         <ul>
-          <li>sajay@gmail.com</li>
-          <li>+91 9825863125</li>
-          <li>77 Masseti vinayak bridge </li>
+          <li>
+            <img src={mail_icon} alt="" />
+            sajay@gmail.com
+          </li>
+          <li>
+            <img src={phone_icon} alt="" />
+            +91 9825863125
+          </li>
+          <li>
+            <img src={location_icon} alt="" />
+            77 Masseti vinayak bridge{" "}
+          </li>
         </ul>
       </div>
-      <div className="contact-col"></div>
+      <div className="contact-col">
+        <form onSubmit={onSubmit}>
+          <label>Your Name</label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Enter your name"
+            required
+          />
+          <label>Phone Number</label>
+          <input
+            type="text"
+            name="phone"
+            placeholder="Enter your mobile no."
+            required
+          />
+          <label>Write your messages here</label>
+          <textarea
+            name="message"
+            rows="6"
+            placeholder="Enter your message"
+            required
+          ></textarea>
+          <button type="submit" className="btn dark-btn">
+            Submit
+            <img src={white_arrow} alt="" />
+          </button>
+        </form>
+        <span>{resultMessage}</span>
+      </div>
     </div>
   );
 };
