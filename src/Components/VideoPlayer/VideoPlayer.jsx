c6c8c732befaa68a3a5796620a3021125b542ed1
Ajay Sahani <ajay.sahani@ehubexperts.com>
2024-07-17 13:57:54 +0530

    insert video in about section

--- src/Components/VideoPlayer/VideoPlayer.jsx
+++ src/Components/VideoPlayer/VideoPlayer.jsx
@@ -1,10 +1,19 @@
 import React from "react";
-import "./VideoPlayer.css";
+// import "./VideoPlayer.css";
 
 const VideoPlayer = () => {
   return (
     <div className="video-player">
-      <video src="" autoPlay muted controls></video>
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/H8Lyj2D_cWo?si=fD545blh3bGNsE7u"
+        title="YouTube video player"
+        // frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+      ></iframe>
     </div>
   );
 };
